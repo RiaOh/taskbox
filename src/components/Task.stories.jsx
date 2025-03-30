f135711b80d36765c674472d5b3f33581ae35b6d
RiaOh <ken5840714@gmail.com>
2025-03-30 18:31:54 +0900

    first commit

---
+++ src/components/Task.stories.jsx
@@ -0,0 +1,47 @@
+import { fn } from "@storybook/test";
+
+import Task from "./Task";
+
+export const ActionsData = {
+  onArchiveTask: fn(),
+  onPinTask: fn(),
+};
+
+export default {
+  component: Task, // 컴포넌트
+  title: "Task",
+  tags: ["autodocs"],
+  //👇 "Data"로 끝나는 export들은 스토리가 아닙니다.
+  excludeStories: /.*Data$/,
+  args: {
+    ...ActionsData,
+  },
+};
+
+export const Default = {
+  args: {
+    task: {
+      id: "1",
+      title: "Test Task",
+      state: "TASK_INBOX",
+    },
+  },
+};
+
+export const Pinned = {
+  args: {
+    task: {
+      ...Default.args.task,
+      state: "TASK_PINNED",
+    },
+  },
+};
+
+export const Archived = {
+  args: {
+    task: {
+      ...Default.args.task,
+      state: "TASK_ARCHIVED",
+    },
+  },
+};
